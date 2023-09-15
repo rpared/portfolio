df4db931abc7ba8b9f0c20c48f132dae9cd70db8
Roger Paredes <115596517+rpared@users.noreply.github.com>
2023-09-15 10:54:22 -0400

    Add files via upload

--- web.js
+++ web.js
@@ -48,70 +48,15 @@ function costravid(){
 }
 
 
-document.getElementById("rcbbtn").addEventListener("click", function(){
+function produ(){
     popUp.style.display = "flex";
-    portMode = "rcb";
-    popUpImg.src = "images/portfoliolargepics/rcb1.png";
-    
-});
+    portMode = "produ";
+    popUpImg.src = "images/portfoliolargepics/produ1.png"; 
+};
 
-document.getElementById("yachaybtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "yachay";
-    popUpImg.src = "images/portfoliolargepics/yachay1.png"; 
-});
-
-document.getElementById("apexbtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "apex";
-    popUpImg.src = "images/portfoliolargepics/apex2.png"; 
-});
-
-document.getElementById("mardisbtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "mardis";
-    popUpImg.src = "images/portfoliolargepics/mardis1.png"; 
-});
-
-document.getElementById("age-resurgencebtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "age";
-    popUpImg.src = "images/portfoliolargepics/age1.png"; 
-});
-
-document.getElementById("pantallabtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "pantalla";
-    popUpImg.src = "images/portfoliolargepics/pantalla1.png"; 
-});
-
-document.getElementById("seastarbtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "seastar";
-    popUpImg.src = "images/portfoliolargepics/seastar1.png"; 
-});
-
-document.getElementById("ecusouthbtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "ecusouth";
-    popUpImg.src = "images/portfoliolargepics/ecusouth1.png"; 
-});
-
-document.getElementById("reflexbtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "reflex";
-    popUpImg.src = "images/portfoliolargepics/reflex1.jpg"; 
-});
-
-document.getElementById("rumibtn").addEventListener("click", function(){
-    popUp.style.display = "flex";
-    portMode = "rumi";
-    popUpImg.src = "images/portfoliolargepics/rumi1.png"; 
-});
 
 
 // Pop Up Buttons Functions
-// NEXT BUTTON
 function nextSlide(){
     if (portMode == "produ"){
         if (popUpImg.src.endsWith("produ1.png")){
@@ -228,6 +173,19 @@ function nextSlide(){
 };
 
 // PREV BUTTON
+// function for web&It
+function prevSlide(){
+    if (portMode == "produ"){
+        if (popUpImg.src.endsWith("produ1.png")){
+        popUpImg.src = "images/portfoliolargepics/produ3.png";
+        } else if (popUpImg.src.endsWith("produ3.png")){
+        popUpImg.src = "images/portfoliolargepics/produ2.png";
+        } else if (popUpImg.src.endsWith("produ2.png")){
+            popUpImg.src = "images/portfoliolargepics/produ1.png";
+            }
+        }
+};
+
 prevBtn.addEventListener("click", function(){
 
     if (portMode == "rcb"){
