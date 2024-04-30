17c28e7177ee595a00434de96539636f8508e280
Roger Paredes <115596517+rpared@users.noreply.github.com>
2024-04-30 11:40:01 -0400

    Add files via upload

--- password.js
+++ password.js
@@ -1,33 +1,32 @@
 let password;
-password = "roger";
+password = "frida";
 let password1 = "tasha";
 let response;
 let entryCount = 0;
 let entryLimit = 3;
 let error = false;
 
-
-function resumePassword(){
-    
-    while(response != password || ! password1 && !error && !null){
-        if(entryCount < entryLimit){
-             response = window.prompt(`Enter Password`);
-             entryCount++;
-        } else {error = true;}
-        if (response === null){ entryCount = 0; error = false; return;}
+function resumePassword() {
+  while (response != password || (!password1 && !error && !null)) {
+    if (entryCount < entryLimit) {
+      response = window.prompt(`Enter Password`);
+      entryCount++;
+    } else {
+      error = true;
     }
-   
-    if(error){
-        alert("Contact Roger for password: rparedes@costra.ec");
-        entryCount = 0;
-        error = false;
-   } else {
-    window.open("resume/Resume2023_RogerParedes.pdf","_blanc");
-   }
-   return;
-   
+    if (response === null) {
+      entryCount = 0;
+      error = false;
+      return;
+    }
+  }
 
+  if (error) {
+    alert("Contact Roger for password: rparedes@costra.ec");
+    entryCount = 0;
+    error = false;
+  } else {
+    window.open("resume/Resume2024_RogerParedes.pdf", "_blanc");
+  }
+  return;
 }
-
-
-
